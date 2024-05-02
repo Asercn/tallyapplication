b658f58ec7b8b85795ef0a9ad6a1ebaa2d6fa644
Asercn <534304548@qq.com>
2024-05-02 22:42:56 +0800

    完成首页数据总图的页面

--- entry/src/main/ets/common/utils/DateUtil.ts
+++ entry/src/main/ets/common/utils/DateUtil.ts
@@ -1,5 +1,6 @@
 class DateUtil{
 
+
   formatDate(num: number): string{
     let date = new Date(num)
     let year = date.getFullYear()
@@ -13,6 +14,24 @@ class DateUtil{
   beginTimeOfDay(date: Date){
     let d = new Date(date.getFullYear(), date.getMonth(), date.getDate())
     return d.getTime()
+  }
+
+
+  getWeek(date:Date):number{
+    let today:any = new Date(date)
+    // 获取到本月的第一天
+    let firstDayOfMonth:any = new Date(today.getFullYear(), today.getMonth(), 1);
+    // 获取本月的第一周的第一天（如果本月的第一天不是周一，则向前推移直到找到周一）
+    while (firstDayOfMonth.getDay() !== 1) {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 1);
+    }
+
+    // 计算今天距离本月第一周的天数
+    let daysSinceFirstWeek:number = Math.floor((today - firstDayOfMonth) / (1000 * 60 * 60 * 24));
+    // 计算今天是本月的第几周
+    var weekNumber:number = Math.ceil((daysSinceFirstWeek + 1) / 7);
+    return weekNumber;
+
   }
 }
 
